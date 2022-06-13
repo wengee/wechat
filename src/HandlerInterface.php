edc14674a9114a7ddd2aa8a9dc796138e1209945
Fung Wing Kit <wengee@gmail.com>
2022-06-13 14:59:26 +0800

    commit

--- src/HandlerInterface.php
+++ src/HandlerInterface.php
@@ -1,14 +1,12 @@
 <?php declare(strict_types=1);
 /**
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2020-06-03 17:15:25 +0800
+ * @version  2022-06-13 14:59:17 +0800
  */
 
 namespace fwkit\Wechat;
 
-use fwkit\Wechat\Message\MessageBase;
-
 interface HandlerInterface
 {
-    public function __invoke(ClientBase $wechat, MessageBase $message, callable $next);
+    public function __invoke(ClientBase $wechat, $message, callable $next);
 }
